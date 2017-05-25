fb963dbe7996654da28698245391545908a43983
Greg Rubino <bibil.thaysose@gmail.com>
2017-05-24 22:15:00 -0400

    update package.json and use environment for server

--- server/swagger_server/__main__.py
+++ server/swagger_server/__main__.py
@@ -3,11 +3,11 @@
 import connexion
 from .encoder import JSONEncoder
 from flask.ext.cors import CORS
-
+import os
 
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'illustrate the intersection of properties of database objects'})
     CORS(app.app)
-    app.run(port=8080)
+    app.run(host=os.getenv('COCURRENT_HOST', '0.0.0.0'), port=os.getenv('COCURRENT_PORT', 8080))
