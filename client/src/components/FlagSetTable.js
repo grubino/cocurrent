9c13ee165690dd497126e4cdec7b3ab759c085e5
Greg Rubino <bibil.thaysose@gmail.com>
2017-05-29 02:56:01 -0400

    export csv button

--- client/src/components/FlagSetTable.js
+++ client/src/components/FlagSetTable.js
@@ -9,6 +9,18 @@ import React from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 class FlagSetTable extends React.Component {
+  export(cols, rows) {
+    let allCols = ['id'].concat(cols)
+    let firstRow = allCols.join(',');
+    let csvRows = rows.map(row => allCols.reduce((acc, x) => acc.concat(row[x]), []).join(',')).join('\n');
+    let text = [firstRow, csvRows].join('\n');
+    let blob = new Blob([text], {type: 'text/csv;charset=utf-8'});
+    let url = window.URL.createObjectURL(blob);
+    let tempLink = document.createElement('a');
+    tempLink.href = url;
+    tempLink.setAttribute('download', 'export.csv');
+    tempLink.click();
+  }
   render() {
     let total = this.props.total;
     let cols = this.props.columns;
@@ -20,36 +32,50 @@ class FlagSetTable extends React.Component {
       });
       return r;
     });
-    return (
-      <BootstrapTable
-        data={ rows }
-        height='200px'
-        striped
-        hover
-        condensed
-        scrollTop={ 'Bottom' }
-        selectRow={
-          {
-            mode: 'checkbox',
-            clickToSelect: true
-          }
-        }>
-        <TableHeaderColumn
-          dataField='id'
-          width='200px'
-          isKey={true}>ID</TableHeaderColumn>
-        {
-          cols.map(col => {
-            return (
-              <TableHeaderColumn
-                key={col}
-                width='200px'
-                dataField={`${col}`}>{col}</TableHeaderColumn>
-            );
-          })
-        }
-      </BootstrapTable>
-    );
+    if (rows.length > 0) {
+      return (
+        <div>
+          <button onClick={ () => this.export(cols, rows) }>export csv</button>
+          <BootstrapTable
+            data={ rows }
+            height='200px'
+            striped
+            hover
+            condensed
+            scrollTop={ 'Bottom' }
+            options={
+              {
+                withoutNoDataText: true
+              }
+            }
+            selectRow={
+              {
+                mode: 'checkbox',
+                clickToSelect: true
+              }
+            }>
+            <TableHeaderColumn
+              dataField='id'
+              width='200px'
+              isKey={true}>ID</TableHeaderColumn>
+            {
+              cols.map(col => {
+                return (
+                  <TableHeaderColumn
+                    key={col}
+                    width='200px'
+                    dataField={`${col}`}>{col}</TableHeaderColumn>
+                );
+              })
+            }
+          </BootstrapTable>
+        </div>
+      );
+    } else {
+      return (
+        <p style={ {justifyContent: 'center'} }>There is no data to show</p>
+      );
+    }
   }
 }
 
