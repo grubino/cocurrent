1f59a7e25cffc9d99b0e7566c035f61cbf1e1730
Greg Rubino <bibil.thaysose@gmail.com>
2017-05-30 11:24:10 -0400

    commenting profiling middleware

--- server/swagger_server/__main__.py
+++ server/swagger_server/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import connexion
-from werkzeug.contrib.profiler import ProfilerMiddleware
+#from werkzeug.contrib.profiler import ProfilerMiddleware
 
 from .encoder import JSONEncoder
 from flask_cors import CORS
@@ -10,8 +10,9 @@ import os
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = JSONEncoder
-    app.app.config['PROFILE'] = True
-    app.app.wsgi_app = ProfilerMiddleware(app.app.wsgi_app, restrictions=[30])
+    # For profiling
+    #app.app.config['PROFILE'] = True
+    #app.app.wsgi_app = ProfilerMiddleware(app.app.wsgi_app, restrictions=[30])
     app.add_api('swagger.yaml', arguments={'title': 'illustrate the intersection of properties of database objects'})
     CORS(app.app)
     app.run(host=os.getenv('COCURRENT_HOST', 'localhost'), port=os.getenv('COCURRENT_PORT', 8080))
