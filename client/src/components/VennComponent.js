7480ea7b365eef85e62420a4581d3506552d23ca
Greg Rubino <bibil.thaysose@gmail.com>
2021-12-24 14:52:15 -0500

    use existing element reference

--- client/src/components/VennComponent.js
+++ client/src/components/VennComponent.js
@@ -73,7 +73,7 @@ const VennComponent = createReactClass({
     const genText = function(d) {
       return `${(100 * (d.size / totalPopulation)).toFixed(2)}`;
     };
-    d3.selectAll('.venn-circle path')
+    chart(div)
       .style('fill-opacity', 0)
       .style('stroke-width', 2)
       .style('stroke', '#444');
